b6e159f9b25f1925e60886f3531097891aaf1843
Victor Iris <victormidp@gmail.com>
2021-05-11 21:21:43 -0500

    test(cards): add check of generated cards

--- cards/deck.go
+++ cards/deck.go
@@ -14,7 +14,7 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "CLubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
