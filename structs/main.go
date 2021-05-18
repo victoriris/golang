298678b0a78962b5fda52cebb8200d2177928f48
Victor Iris <victormidp@gmail.com>
2021-05-17 22:23:49 -0500

    feat: add structs example

---
+++ structs/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "jim",
+		lastName:  "joe",
+		contact: contactInfo{
+			email:   "jim@jim.me",
+			zipCode: 123456,
+		},
+	}
+	jim.updateName("Jil")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
